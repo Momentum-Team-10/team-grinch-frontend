e7d223236c0dfb18b957916f41115a8fbe8d456a
SucioLoco <irelandrj@yahoo.com>
2021-12-16 14:51:55 -0500

    Added requestLogin component

---
+++ src/components/requestLogin.js
@@ -0,0 +1,12 @@
+export default function requestLogin(email, password) {
+    return fakeResponse(email, password)
+}
+
+function fakeResponse(email, password) {
+    return new Promise((resolve, reject) => {
+        if (email && password) {
+            resolve({ auth_token: 'faketoken12345678'})
+        }
+        reject(new Error('Incorrect Information'))
+    })
+}
