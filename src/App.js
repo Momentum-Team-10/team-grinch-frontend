99f18f0f1f9cf6b426611e097bdafb7bde85c9f5
Amy Gori <amydgori@gmail.com>
2021-12-21 16:25:48 -0500

    Sort out request and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import Login from './components/Login';
-import { useState, useEffect} from 'react';
+import Login from './components/Login'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
-import questbox from'./questbox.png';
-import './App.css';
-import { QuestionCard } from './components/QuestionCard';
+
+import './App.css'
+import { QuestionList } from './components/QuestionList'
 // import NavBar from '.components/NavBar';
 
-let questUrl = "https://questbox-app.herokuapp.com/api/questions"
+let questUrl = 'https://questbox-app.herokuapp.com/api/questions'
 
 export default function App() {
   const [email, setEmail] = useState('')
   const [token, setToken] = useState('')
-  let [QuestionList, setQuestionList] = useState(null)
-  
-  useEffect(() => {
-    axios
-    .get(questUrl)
-    .then((response ) => {
-        setQuestionList(response.data)
-    })
-}, []
-)
-  
+
   function setAuth(email, token) {
     setEmail(email)
     setToken(token)
@@ -29,22 +19,9 @@ export default function App() {
 
   const isLoggedIn = email && token
 
-  
-  return (
-  <>
-    <img className="logo" src={questbox} alt="Questbox" />
-    {isLoggedIn ? (
-        <>
-          <QuestionCard pk={QuestionList.pk} title={QuestionList.title} body={QuestionList.body} author={QuestionList.author} tags={QuestionList.tags} favorited_by={QuestionList.favorited_by}>
-          </QuestionCard>  
-        </>
-      ) : (
-        <Login setAuth={setAuth} isLoggedIn={isLoggedIn} />
-      )}
+  if (!isLoggedIn) {
+    return <Login setAuth={setAuth} isLoggedIn={isLoggedIn} />
+  }
 
-    </>
-  )
+  return <QuestionList />
 }
-
-
-
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,26 +1,36 @@
-// import { useState, useEffect } from "react";
-// // import moment from 'moment';
-// import { QuestionCard } from "./QuestionCard";
-// import axios from 'axios'
+import { useState, useEffect } from 'react'
+// import moment from 'moment';
+import { QuestionCard } from './QuestionCard'
+import axios from 'axios'
+import questbox from '../questbox.png'
 
-// let questUrl = "https://questbox-app.herokuapp.com/api/questions"
+let questUrl = 'https://questbox-app.herokuapp.com/api/questions'
 
+export const QuestionList = () => {
+  const [questionList, setQuestionList] = useState(null)
 
-// export const QuestionList = () => {
-//    const [questionList, setQuestionList] = useState(null)
+  useEffect(() => {
+    axios.get(questUrl).then((response) => {
+      setQuestionList(response.data)
+    })
+  }, [])
 
-    
-// useEffect(() => {
-//     axios
-//     .get(questUrl)
-//     .then((response ) => {
-//         setQuestionList(response.data)
-//     })
-// }, []
-// )
-// return(
-//     <>
-//         <
-//     </>
-// )
-// }
+  return (
+    <>
+      <img className="logo" src={questbox} alt="Questbox" />
+      {questionList &&
+        questionList.map((question) => {
+          return (
+            <QuestionCard
+              pk={question.pk}
+              title={question.title}
+              body={question.body}
+              author={question.author}
+              tags={question.tags}
+              favorited_by={question.favorited_by}
+            />
+          )
+        })}
+    </>
+  )
+}
