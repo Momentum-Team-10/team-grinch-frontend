eff02d9e211cad7a73c2678ccf857eea1309dcc7
SucioLoco <irelandrj@yahoo.com>
2021-12-21 19:00:51 -0500

    questions rendering to page

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
-// import moment from 'moment';
+
 
 export const QuestionCard = (props) => {
     let {title, body, author, tags, favorited_by} = props
     return (
-        <div>
+        <div className="question-card">
             <h3>{title}</h3>
             <p>author: {author}</p>
             <p>{body}</p>
