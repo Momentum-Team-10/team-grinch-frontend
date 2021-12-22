fddc9f1e63f70d6b7ff9cd1f131702ca658e5db4
Jordyn <jordyn@example.com>
2021-12-21 22:17:34 -0500

    correcting errors

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import moment from 'moment';
 
-export const QuestionCard = ({ question }) => {
+export const QuestionCard = (props) => {
+    let { title, author, body } = props
     return (
         <div>
-            <h3>{question.title}</h3>
-            <p>author: {question.author}</p>
-            <p>{question.description}</p>
-            <p>tags: {question.tags.map(tag => <p>{tag}</p>)}</p>
-            <p>published: {moment(question.created_at).format('DD/MM/YYYY')}</p>
-            <p>{question.favorited_by.length} likes </p>
+            <h3>{title}</h3>
+            <p>author: {author}</p>
+            <p>{body}</p>
+            {/* <p>tags: {tags.map(tag => <p>{tag}</p>)}</p>
+            <p>published: {moment(created_at).format('DD/MM/YYYY')}</p>
+            <p>{favorited_by.length} likes </p> */}
         </div>
     )
 
